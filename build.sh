e2465eb7c235d2e5f4411972f30dd95d1651019c
Rob Hardwick <robhardwick@gmail.com>
2015-02-03 19:48:04 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# For homebrew on os x
+export CMAKE_PREFIX_PATH="/usr/local/opt/qt5:/usr/local/opt/luajit"
+
+# Paths
+CWD=`pwd`
+BUILD="./build"
+
+# Clean build dir and exit
+if [[ $1 == "clean" ]]; then
+    rm -r $BUILD
+    exit
+fi
+
+# Create build dir
+if [ ! -d "$BUILD" ]; then
+    mkdir $BUILD
+fi
+
+# Enter tmp dir and build
+cd $BUILD
+cmake -DCMAKE_BUILD_TYPE=debug ..
+make
+cd $CWD
+
+# Run app
+$BUILD/app/EvoMu.app/Contents/MacOS/EvoMu;
