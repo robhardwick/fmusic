c9e6582311f4285395a04053545544fe40fde25f
Rob Hardwick <robhardwick@gmail.com>
2015-02-11 00:19:48 +0000

    Only install deps on Travis CI

--- Vagrantfile
+++ Vagrantfile
@@ -4,8 +4,21 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "http://files.vagrantup.com/precise64.box"
   config.vm.provision "shell", inline: <<-SHELL
+    # Update
+    sudo apt-get update -qq
+    sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq -o Dpkg::Options::="--force-confnew"
+
     # Install dependencies
     /vagrant/scripts/deps.sh
+
+    # Add toolchain repo for GCC 4.8
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+
+    # Install GCC 4.8
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq build-essential cmake gcc-4.8 g++-4.8
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
     # Clean up
     sudo apt-get clean
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
 
-# Update
-sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq -o Dpkg::Options::="--force-confnew"
-
 # Install "add-apt-repository" command
 sudo apt-get install -qq python-software-properties
 
 # Add SDK repo for QT5
 sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
-
-# Add toolchain repo for GCC 4.8
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-
-# Update
 sudo apt-get update -qq
 
 # Install all dependencies
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq \
-    build-essential cmake gcc-4.8 g++-4.8 \
     qtbase5-dev qt5-default qttools5-dev-tools \
     libluajit-5.1-dev librtmidi-dev \
     jackd
-
-# Set GCC 4.8 as default
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
