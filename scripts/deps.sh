5cca3aeb6c8e5e0189f3443b88a930df707e8bfb
Rob Hardwick <robhardwick@gmail.com>
2015-02-11 00:21:48 +0000

    Show install logs

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 
 # Install all dependencies
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install \
     qtbase5-dev qt5-default qttools5-dev-tools \
     libluajit-5.1-dev librtmidi-dev \
     jackd
